d7df122fa9f30d3969b46bc7b0acb1823f014ed2
Remco Veldkamp <remcoder@gmail.com>
2014-05-17 20:58:38 +0200

    fixed negative time deltas. refactored timer.

---
+++ sensorhub/client/lib/timer.js
@@ -0,0 +1,71 @@
+
+var SECOND  = 1000,
+    MINUTE  = 60 * SECOND,
+    HOUR    = 60 * MINUTE,
+    DAY     = 24 * HOUR,
+    WEEK    = 7  * DAY;
+
+var lookup = {
+      "second"  : SECOND,
+      "minute"  : MINUTE,
+      "hour"    : HOUR,
+      "day"     : DAY,
+      "week"    : WEEK
+};
+
+function granularity(g) {
+  if (typeof g == 'number')
+    return g;
+
+  if (typeof g == 'string') {
+    var key = g
+      .toLowerCase()
+      .replace(/s$/,''); // remove potential 's' at the end.
+
+    return lookup[key]
+  }
+  
+  throw new Error('granularity should be either string or number');
+}
+
+function Timer(opts) {
+  opts = opts || {};
+  this.granularity = granularity(opts.granularity)
+  this.interval = opts.interval || this.granularity / 20;
+  console.log('granularity', this.granularity);
+  console.log('interval', this.interval);
+  this.currentTime = 0;
+  this.dep = new Deps.Dependency;
+}
+
+Timer.prototype.getTime = function() {
+  this.dep.depend();
+  return this.currentTime;
+}
+
+Timer.prototype.setTime = function(t) {
+  console.log('checking time')
+  if (Math.round(this.currentTime/this.granularity) != Math.round(t / this.granularity)) {
+    console.log('setting time')
+    this.currentTime = t;
+    this.dep.changed();
+  }
+}
+
+Timer.prototype.start = function() {
+  this.currentTime = new Date;
+  this.timer = setInterval(function() {
+    this.setTime(new Date);
+  }.bind(this), this.interval);
+}
+
+Timer.prototype.stop = function() {
+  clearInterval(this.timer);
+}
+
+// export as global
+Chronos = {
+  createTimer : function (opts) {
+    return new Timer(opts);
+  }
+}
--- sensorhub/sensorhub.js
+++ sensorhub/sensorhub.js
@@ -4,79 +4,74 @@ if (Meteor.isClient) {
     Meteor.subscribe('Measurements');
   });
 
-  var _prevTimestamp = 0;
-  var _currentTime = 0;
-  var _currentTimeDep = new Deps.Dependency;
-
-
-  var getCurrentTime = function () {
-    _currentTimeDep.depend()
-    return _currentTime;
-  };
-
-  var setCurrentTime = function (t) {
-    _currentTime = t;
-    _currentTimeDep.changed();
-  };
-
-
-  setInterval(function() {
-    setCurrentTime(+new Date);
-  }, 300);
+  var prev = 0;
+  var prevTimeStamp = 0;
+  var timer = Chronos.createTimer({ granularity : "second" });
+  timer.start();
 
   Template.sensor.data = function () {
+    console.log('sensor.data')
     var data = Sensors.findOne({  _sensorId: 'woonkamer' }) || {};
-    data['_currentTime'] = getCurrentTime();
+    var now = data['_currentTime'] = +timer.getTime();
+    
 
-    if (data._timestamp != _prevTimestamp ) {
-      _prevTimestamp = data._timestamp
+    if (data._timestamp != prevTimeStamp ) {
+      prevTimeStamp = data._timestamp;
+      var updated = true;
     }
-    var timeDiff = new Date - _prevTimestamp;
+    
+    var timeDiff = now - prev;
     data['_timeDiff'] = timeDiff;
-    data['timeAgo'] = timeDiff < 20000 ? Math.round(timeDiff/1000) + "s ago" : moment(Math.min(_prevTimestamp, +new Date)).fromNow(); // prevent negative numbers b/c that would be weird. They're the result of having differing clocks on Pi and Meteor
+    data['timeAgo'] = Math.round(timeDiff/1000) + "s ago" ;
+
+    // timeDiff < 20000 
+    //   ? Math.round(timeDiff/1000) + "s ago" 
+    //   : moment(prev).fromNow();
+
     var color = mapTo(timeDiff, 0, 60000, 0, 255); //Math.max(255 * (timeDiff / 60000));
     var alpha = 1 - mapTo(timeDiff, 0, 60000, 0, 0.5);
     data['_color'] = "rgba(" +Math.round(color)+ ",0,0, "+alpha+")";
+
+    console.log(now, timeDiff);
+    if (updated)
+      prev = now;
     return data;
   };
 
-  Template.history.rendered = function() {
-    var canvas = this.findAll('canvas')[0];
-    var ctx = canvas.getContext('2d');
-    // console.log('rendered', canvas);
+  // Template.history.rendered = function() {
+  //   var canvas = this.findAll('canvas')[0];
+  //   var ctx = canvas.getContext('2d');
+  //   // console.log('rendered', canvas);
 
-    Deps.autorun(function() {
-      var filtered = Measurements
-      .find({ temperature : { $ne : null} })
-      .fetch()
-      .slice(0,30);
+  //   Deps.autorun(function() {
+  //     var filtered = Measurements
+  //     .find({ temperature : { $ne : null} }, { limit: 30})
+  //     .fetch();
         
-      var data = {
-        labels : filtered.map(function(f,i) { return i; }),
-        datasets : [{
-          fillColor : "rgba(220,220,220,0.5)",
-          strokeColor : "rgba(220,220,220,1)",
-          pointColor : "rgba(220,220,220,1)",
-          pointStrokeColor : "#fff",
-          data : filtered.map(function(f) { return f.temperature; }),
-        }]
-      };  
+  //     var data = {
+  //       labels : filtered.map(function(f,i) { return i; }),
+  //       datasets : [{
+  //         fillColor : "rgba(220,220,220,0.5)",
+  //         strokeColor : "rgba(220,220,220,1)",
+  //         pointColor : "rgba(220,220,220,1)",
+  //         pointStrokeColor : "#fff",
+  //         data : filtered.map(function(f) { return f.temperature; }),
+  //       }]
+  //     };  
       
-      console.time('chart');
-      var myNewChart = new Chart(ctx).Bar(data, { 
-        animation: false,
-        scaleOverride: true,
-        scaleSteps: 10,
-        scaleStepWidth: 1,
-        scaleStartValue: 15
-      });
-      console.timeEnd('chart');
+  //     console.time('chart');
+  //     var myNewChart = new Chart(ctx).Bar(data, { 
+  //       animation: false,
+  //       scaleOverride: true,
+  //       scaleSteps: 10,
+  //       scaleStepWidth: 1,
+  //       scaleStartValue: 15
+  //     });
+  //     console.timeEnd('chart');
 
-      //console.log(filtered);
-    })
-    
-    
-  }
+  //     //console.log(filtered);
+  //   });
+  // }
 }
 
 function mapTo (v, s0,s1, t0,t1) {
